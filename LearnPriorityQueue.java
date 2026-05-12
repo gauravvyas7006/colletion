55a788d3d4b6cecf74cb60d251d3c65fb8c948c5
gaurav <vyashaurav7006@gmail.com>
2026-05-13 04:34:57 +0530

    LEarnPriority

--- LearnPriorityQueue.java
+++ LearnPriorityQueue.java
@@ -1,16 +1,27 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class LearnPriorityQueue {
     public static void main(String[] args) {
-        Queue<Integer> pq = new PriorityQueue<>();
+
+        Comparator<Integer> com = new Comparator<Integer>() {
+            public int compare(Integer i, Integer j) {
+                if (i  > j) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+
+        Queue<Integer> pq = new PriorityQueue<>(com);
+
         pq.offer(40);
         pq.offer(30);
         pq.offer(10);
         pq.offer(80);
+
         System.out.println(pq);
-        pq.poll();
-        System.out.println(pq);
-        System.out.println(pq.peek());
     }
 }
